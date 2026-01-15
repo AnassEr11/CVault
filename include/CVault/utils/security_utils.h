33202c1d3a6d1149d3502c942bc0678c3cd56d04
AnassEr11 <anasseroua224@gmail.com>
2026-01-15 22:23:37 +0100

    feat(utils): implement security utilities
    - Define standard return codes and charset flags for utility operations
    - Add security_utils.h header with comprehensive utility function declarations
    - Implement secure memory wiping (secure_memset) to prevent compiler optimizations
    - Implement UUID v4 generation for unique identifier creation
    - Implement cryptographic password generation with configurable character sets
    - Implement clipboard copy utility with automatic secure clearing after timeout
    - Remove placeholder .gitkeep files from utils directories
    
    This module provides foundational security utilities required by the encryption
    and key management subsystems.

---
+++ include/CVault/utils/security_utils.h
@@ -0,0 +1,99 @@
+#ifndef SECURITY_UTILS_H
+#define SECURITY_UTILS_H
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#define UUID_STR_LEN 36
+#define TIMEOUT      15
+#define MAX_LEN      200
+
+/**
+ * @brief: Enumeration for return codes
+ */
+typedef enum  {
+	SUCCESS = 0, /* Operation completed successfully */
+	NOT_SUPPORTED, /* Operation is not supported for the current environment */
+	NULL_POINTER, /* A required pointer argument is NULL*/
+	INVALID_SIZE, /* A length argument exceeds limits (0,MAX_LEN) */
+	SYSCALL_ERR, /*	A system call failed */
+	UNEXPECTED_ERR, /* A generic Failure */
+	NOT_FOUND /* A required dependency is not fount */
+} util_result_code;
+
+/**
+ * @brief: Flags to determine the set used to generate generate
+ */
+typedef enum{
+    CHARSET_FULL,
+    CHARSET_ALPHANUM,
+    CHARSET_ALPHA,
+    CHARSET_UPPER,
+    CHARSET_LOWER,
+    CHARSET_DIGITS_SYMBOLS,
+    CHARSET_SYMBOLS,
+    CHARSET_DIGITS
+} charset_flag;
+
+/**
+ * @brief: Securely wipes memory by bypassing compiler optimizations.
+ *
+ * @param: ptr The memory block to clear.
+ * @param: width The number of bytes to clear.
+ *
+ * @return: SUCCESS on success, 
+ * NULL_POINTER on passing null to ptr,
+ * INVALID_SIZE on passing an invalid width (too big or negative).
+ */
+util_result_code secure_memset(void* ptr, uint64_t width);
+
+/**
+ * @brief: Generates a standard version 4 UUID string
+ *
+ * @param: out_buffer Buffer to store the result
+ *
+ * @return: SUCCESS on success, 
+ * NULL_POINTER on passing NULL to out_buffer, 
+ * SYSCALL_ERR if a system call failed, 
+ * NOT_SUPPORTED if the current environment doesn't support
+ * the implemented system calls
+ *
+ * @note: make sure to pass a pointer that points to at least 37 bytes
+ */
+util_result_code generate_uuid(char* out_buffer);
+
+/**
+ * @brief: Generates a random password
+ *
+ * @param: length Desired length of the password
+ * @param: out_buffer Buffer to store the result
+ * @param: flag The charset_flag determine the charachters set
+ *
+ * @return: SUCCESS on success, 
+ * INVALID_SIZE on passing invalid length, 
+ * NULL_POINTER on passing NULL to out_buffer , 
+ * SYSCALL_ERR if a system call failed, 
+ * NOT_SUPPORTED if the current environment doesn't support
+ * the implemented system calls
+ */
+util_result_code generate_password(uint64_t length, char* out_buffer, 
+								   charset_flag flag);
+/**
+ * @brief: Copies a string to the Wayland clipboard and schedules an auto-clear.
+ *
+ * @param: string The null-terminated string to copy.
+ *
+ * @return: SUCCESS on success,
+ * NOT_FOUND if wl-copy tool isn't installed in the environment
+ * or the environment isn't on Wayland,
+ * SYSCALL_ERR if a system call failed,
+ * UNEXPECTED_ERR if an lib function crashes for not knows reason,
+ * NOT SUPPORTED if the current environment doesn't support
+ * the implemented system calls
+ *
+ * @note: This function forks a background process that will clear 
+ * the clipboard after TIMEOUT seconds
+ */
+util_result_code copy_string_to_clipboard(char* string);
+
+#endif
---
+++ src/utils/security_utils.c
@@ -0,0 +1,205 @@
+#include <CVault/utils/security_utils.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#if defined(__linux__)
+    #include <sys/random.h>
+    #include <unistd.h>
+	#include <signal.h>
+	#include <sys/wait.h>
+#endif
+
+util_result_code secure_memset(void* ptr, uint64_t width){
+	if(!ptr){
+		return NULL_POINTER;
+	}
+
+	if(width <= 0 || width > MAX_LEN ){
+		return INVALID_SIZE;
+	}
+
+	volatile uint8_t* p = (volatile uint8_t*)ptr;
+
+	while (width--) {
+		*p++ = 0;
+	}
+
+	return SUCCESS;
+}
+
+util_result_code generate_uuid(char* out_buffer){
+#if defined(__linux__)
+
+	if(!out_buffer){
+		return NULL_POINTER;
+	}
+
+	uint8_t random_bytes[16];
+
+	if(getrandom(random_bytes,16,0) != 16){
+		return SYSCALL_ERR;
+	}
+
+
+    random_bytes[6] = (random_bytes[6] & 0x0F) | 0x40;
+    random_bytes[8] = (random_bytes[8] & 0x3F) | 0x80;
+
+	snprintf(out_buffer,UUID_STR_LEN+1,
+		     "%02x%02x%02x%02x-%02x%02x-%02x%02x-%02x%02x-%02x%02x%02x%02x%02x%02x",
+		     random_bytes[0],random_bytes[1],random_bytes[2],random_bytes[3],
+		     random_bytes[4],random_bytes[5],random_bytes[6],random_bytes[7],
+		     random_bytes[8],random_bytes[9],random_bytes[10],random_bytes[11],
+		     random_bytes[12],random_bytes[13],random_bytes[14],random_bytes[15]);
+
+	return SUCCESS;
+
+#else
+	// TODO: add portability to other platforms
+	return NOT_SUPPORTED;
+#endif
+}
+
+util_result_code generate_password(uint64_t length, char* out_buffer,
+								   charset_flag flag){
+	if(!out_buffer){
+		return NULL_POINTER;
+	}
+
+	if(length <= 0 || length > MAX_LEN){
+		return INVALID_SIZE;
+	}
+
+	char* charset;
+	int charset_size;
+
+    switch (flag) {
+    default:
+    case CHARSET_FULL:
+        charset = "abcdefghijklmnopqrstuvwxyz"
+                  "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                  "0123456789"
+                  "!@_#)$%=^+&*(-";
+        charset_size = 76;
+        break;
+    case CHARSET_ALPHANUM:
+        charset = "abcdefghijklmnopqrstuvwxyz"
+                  "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                  "0123456789";
+        charset_size = 62;
+        break;
+    case CHARSET_ALPHA:
+        charset = "abcdefghijklmnopqrstuvwxyz"
+                  "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        charset_size = 52;
+        break;
+    case CHARSET_UPPER:
+        charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        charset_size = 26;
+        break;
+    case CHARSET_LOWER:
+        charset = "abcdefghijklmnopqrstuvwxyz";
+        charset_size = 26;
+        break;
+    case CHARSET_DIGITS_SYMBOLS:
+        charset = "0123456789"
+                  "!@#$%^&*()-_=+";
+        charset_size = 24;
+        break;
+    case CHARSET_SYMBOLS:
+        charset = "!@#$%^&*()-_=+";
+        charset_size = 14;
+        break;
+    case CHARSET_DIGITS:
+        charset = "0123456789";
+        charset_size = 10;
+        break;
+    }
+
+#if defined (__linux__)
+    const uint16_t max_rand = 256;
+    const uint16_t bias_threshold = max_rand - (max_rand % charset_size);
+
+    for (uint64_t i = 0; i < length; i++) {
+        uint8_t rand_byte;
+        do {
+            if (getrandom(&rand_byte, 1, 0) != 1) {
+                return SYSCALL_ERR;
+            }
+        } while (rand_byte >= bias_threshold);
+        out_buffer[i] = charset[rand_byte % charset_size];
+    }
+    out_buffer[length] = '\0';
+	return SUCCESS;
+#else
+	// TODO: add portability to other platforms
+	return NOT_SUPPORTED;
+#endif
+}
+
+static pid_t child_process = 0;
+static bool clear_clipboard();
+
+util_result_code copy_string_to_clipboard(char* string) {
+#if defined (__linux__)
+    if (child_process > 0) {
+        if (waitpid(child_process, NULL, WNOHANG) > 0) {
+            child_process = 0;
+        }
+    }
+
+    if (child_process > 0) {
+        if (kill(child_process, 0) == 0) {
+            kill(child_process, SIGKILL);
+            waitpid(child_process, NULL, 0);
+        }
+        child_process = 0;
+    }
+
+    FILE* _pipe = popen("wl-copy", "w");
+    if (!_pipe) {
+        return UNEXPECTED_ERR;
+    }
+	fprintf(_pipe, "%s", string);
+		
+    int status = pclose(_pipe);
+	int exit_code = WEXITSTATUS(status);
+
+	if (exit_code == 127) {
+		return NOT_FOUND;
+	}
+	if (exit_code) {
+		return UNEXPECTED_ERR;
+	}
+
+    pid_t pid = fork();
+
+    if (pid == 0) {
+        sleep(TIMEOUT);
+        if (!clear_clipboard()) {
+            exit(1);
+        }
+        exit(0);
+    } else if (pid > 0) {
+        child_process = pid;
+        return SUCCESS;
+    } else {
+        return SYSCALL_ERR;
+    }
+#else
+	// TODO: add portability to other platforms
+	return NOT_SUPPORTED;
+#endif
+}
+
+
+static bool clear_clipboard()
+{
+    FILE* _pipe = popen("wl-copy", "w");
+    if (!_pipe) {
+        return false;
+    }
+    fprintf(_pipe, "");
+    pclose(_pipe);
+    return true;
+}
